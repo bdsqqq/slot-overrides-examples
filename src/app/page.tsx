aaf86f3371e15c012c9645b7d181358adb84edc3
Igor Bedesqui <igorbedesqui@gmail.com>
2023-06-29 02:10:57 +0100

    feat: tabs in popover

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
+'use client';
 import { Button } from '@/components/ui/button';
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from '@/components/ui/popover';
+import { Tabs, TabsContent } from '@/components/ui/tabs';
+import { useState } from 'react';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <Popover>
-          <PopoverContent>hej do</PopoverContent>
+          <PopoverContent>
+            <GatedContent />
+          </PopoverContent>
           <PopoverTrigger asChild>
             <Button>Open</Button>
           </PopoverTrigger>
@@ -19,3 +24,21 @@ export default function Page() {
     </main>
   );
 }
+
+const GatedContent = () => {
+  const [currentTab, setCurrentTab] = useState<'warn' | 'form'>('warn');
+
+  return (
+    <Tabs value={currentTab} defaultValue="warn">
+      <TabsContent value="warn">
+        Eh, can&apos;t do it man,{' '}
+        <button className="underline" onClick={() => setCurrentTab('form')}>
+          contact admin
+        </button>
+      </TabsContent>
+      <TabsContent value="form">
+        Pretend there&apos;s an useful form for contacting admin here
+      </TabsContent>
+    </Tabs>
+  );
+};
